b3a00671098a2a962589cb989971154b89bac0d2
Jan Cibulka <jan.cibulka@ui.com>
2024-12-02 20:30:42 +0100

    two

--- two/src/main.rs
+++ two/src/main.rs
@@ -39,6 +39,7 @@ fn parse_file_content(content: String) -> Vec<Vec<i32>> {
     }
     lines
 }
+
 fn is_floor_safe(floor: &&Vec<i32>) -> bool {
     println!("{:?}", floor);
     let mut is_floor_ascending: bool = false;
@@ -72,6 +73,24 @@ fn is_floor_safe(floor: &&Vec<i32>) -> bool {
     true
 }
 
+fn create_unsafe_floor_variations(floor: &Vec<i32>) -> Vec<Vec<i32>> {
+    let mut level_variations: Vec<Vec<i32>> = Vec::new();
+    for x in 0..floor.len() {
+        let level_variation: Vec<i32> = floor
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| *i != x) // Exclude the current index
+            .map(|(_, &value)| value) // Collect the remaining values
+            .collect();
+        level_variations.push(level_variation);
+    }
+    level_variations
+}
+
+fn batch_evaluate_floor_safety(floor_variations: Vec<Vec<i32>>) -> bool {
+    floor_variations.iter().filter(is_floor_safe).count() > 0
+}
+
 fn main() -> io::Result<()> {
     let content: String = read_file_content("input.txt")?;
     // let lines = parse_file_content(EXAMPLE_INPUT.to_string());
@@ -79,7 +98,30 @@ fn main() -> io::Result<()> {
 
     // Part 1: Evaluate safety
     let safe_floor_count = lines.iter().filter(is_floor_safe).count();
-
     println!("Part 1 result: {}", safe_floor_count);
+
+    // Part 2: Evaluate safety with damper use
+    let unsafe_floors: Vec<_> = lines
+        .iter()
+        .filter(|floor: &&Vec<i32>| !is_floor_safe(floor))
+        .collect();
+
+    println!("{:?}", unsafe_floors);
+
+    let safe_floors_after_dampening = unsafe_floors
+        .iter()
+        .filter(|floor: &&&Vec<i32>| {
+            batch_evaluate_floor_safety(create_unsafe_floor_variations(floor))
+        })
+        .count();
+    println!(
+        "safe_floors_after_dampening: {}",
+        safe_floors_after_dampening
+    );
+    println!(
+        "Part 2 result: {}",
+        safe_floor_count + safe_floors_after_dampening
+    );
+
     Ok(())
 }
